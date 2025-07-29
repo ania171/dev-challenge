bc7655f3c878364cbcc618749cbc343d0f419759
ania171 <dziedzicz.anna@gmail.com>
2025-07-29 18:49:45 +0200

    Format

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import './App.css';
 import UsersList from './components/UsersList.jsx';
 
 function App() {
-  return (
-    <div className="container mx-auto my-4">
-      <h1 className="text-3xl font-bold">Users list</h1>
-      <UsersList />
-    </div>
-  );
+	return (
+		<div className="container mx-auto my-4">
+			<h1 className="text-3xl font-bold">Users list</h1>
+			<UsersList />
+		</div>
+	);
 }
 
 export default App;
--- src/utils/cn.js
+++ src/utils/cn.js
@@ -3,5 +3,5 @@ import { twMerge } from 'tailwind-merge';
 
 /** Utility function to add Tailwind classes conditionally */
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+	return twMerge(clsx(inputs));
 }
