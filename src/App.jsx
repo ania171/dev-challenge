41593b42c5c2ab08605be6bc8ae2559be202f480
ania171 <dziedzicz.anna@gmail.com>
2025-07-29 19:02:21 +0200

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css';
 import UsersList from './components/UsersList.jsx';
+import Filters from './components/Filters.jsx';
 
 function App() {
 	return (
 		<div className="container mx-auto my-4">
-			<h1 className="text-3xl font-bold">Users list</h1>
-			<UsersList />
+			<h1 className="mb-4 text-3xl font-bold">Users list</h1>
+			<div className="lg:grid lg:grid-cols-3 lg:gap-4 xl:grid-cols-4">
+				<div>
+					<Filters />
+				</div>
+				<div className="lg:col-span-2 xl:col-span-3">
+					<UsersList />
+				</div>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/Filters.jsx
@@ -0,0 +1,5 @@
+function Filters() {
+	return <>Filters</>;
+}
+
+export default Filters;
